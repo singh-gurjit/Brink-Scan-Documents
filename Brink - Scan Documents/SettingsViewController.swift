98455b191f6114435d436e690d8e000cafe461e4
Gurjit Singh <gurjit.singh.engineer@gmail.com>
2020-03-29 22:43:41 +0100

    Update file

---
+++ Brink - Scan Documents/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Brink - Scan Documents
+//
+//  Created by Gurjit Singh on 29/03/20.
+//  Copyright © 2020 Gurjit Singh. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
