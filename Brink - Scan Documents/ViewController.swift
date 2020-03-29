f0fe78ec2bda8dad4e5f8a8e1b699ad3c91ae578
Gurjit Singh <gurjit.singh.engineer@gmail.com>
2020-03-29 22:34:30 +0100

    Initial Commit

---
+++ Brink - Scan Documents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brink - Scan Documents
+//
+//  Created by Gurjit Singh on 29/03/20.
+//  Copyright © 2020 Gurjit Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
